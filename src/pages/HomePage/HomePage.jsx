e4d0ed094a08d568251878f1271b3ac5110bb6a3
Scott Schwab <scottschwab86@gmail.com>
2024-04-18 14:41:09 -0500

    Reduce state to only be aiResponseContent + move requestObj outside of component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,27 +2,28 @@ import "./HomePage.scss";
 import { useEffect, useState } from 'react';
 import { getResponseContent } from '../../utils/openAIcall';
 
+// placeholder requestObj, will later be derived from input forms data
+const requestObj = {
+  messages: [
+    { role: "system", content: "You are a friendly assistant, that gives responses to a community organizer appropriate to LinkedIn in JSON format just post content between 200 and 2000 characters in length, no title. Don't include any extra text outside of the post content itself." },
+    { role: "user", content: "Help me write a professional sounding post about my upcoming community event, suitable for posting on LinkedIn." },
+  ],
+  model: "gpt-3.5-turbo",
+  temperature: 0.02
+}
+
 const Home = () => {
 
-  const [aiResponseContent, setAiResponseContent] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [aiResponseContent, setAiResponseContent] = useState("Loading...");
 
   useEffect(() => {
-    let requestObj = {
-      messages: [
-        { role: "system", content: "You are a friendly assistant, that gives responses to a community organizer appropriate to LinkedIn in JSON format just post content between 200 and 2000 characters in length, no title. Don't include any extra text outside of the post content itself." },
-        { role: "user", content: "Help me write a professional sounding post about my upcoming community event, suitable for posting on LinkedIn." },
-      ],
-      model: "gpt-3.5-turbo",
-      temperature: 0.02
-    }
     const getOpenAIResponse = async () => {
+      setAiResponseContent("Loading...")
 
-        // awaits result of getResponseContent from utils, which makes the API call, before setting the state to that result
-        const responseContent = await getResponseContent(requestObj);
-        // I will rename these
-        setAiResponseContent(responseContent.content ? responseContent.content : responseContent)
-        setLoading(false)
+      const responseContent = await getResponseContent(requestObj);
+
+      // I will rename these, they are the received error or content
+      setAiResponseContent(responseContent.content ? responseContent.content : responseContent)
     }
     getOpenAIResponse();
   }, [])
@@ -31,7 +32,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1 className="home__title"> Welcome !</h1>
-      <p>{loading ? "Loading..." : aiResponseContent}</p>
+      <p>{aiResponseContent}</p>
     </div>
   );
 };
