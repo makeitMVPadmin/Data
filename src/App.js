8f5eb6a2d3598169c5c6ab7e02860860480579e7
Eileen Burdekin <eburdekin@proton.me>
2024-04-18 17:46:47 -0700

    add prompt route to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/HomePage";
+import PromptPage from "./pages/PromptPage/PromptPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/prompt" element={<PromptPage />} />
     </Routes>
   );
 }
