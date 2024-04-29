9737037222be5f6b5f37dd581c160d6b00d7f694
aborgono <albertoiborgonovo@gmail.com>
2024-04-29 17:28:28 -0400

    added main function to dashboard page

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ExperienceGraph from '../../components/Example/experienceCard';
 import Modal from 'react-modal'; // Import if using react-modal
 import './DashboardPage.scss';
+import {main} from '../../functions/fetchUserData'
 
 Modal.setAppElement('#root'); // Properly hide app content for screen readers
 
 function DashboardPage() {
     const [modalIsOpen, setModalIsOpen] = useState(false); // State to manage modal visibility
+    const [userData, setUserData] = useState(null)
 
     const openModal = () => {
         setModalIsOpen(true);
@@ -15,6 +17,17 @@ function DashboardPage() {
     const closeModal = () => {
         setModalIsOpen(false);
     };
+
+    useEffect(() => {
+        main()
+            .then(data => {
+                setUserData(data)
+                console.log('User data fetched:', data)
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error)
+            })
+    }, [])
 
     return (
         <div>
